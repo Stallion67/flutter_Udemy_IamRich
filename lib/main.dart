2ab925b4f415a14499c1cca194ccfa8ea87c08eb
Ben Okello Mwaka <mwakaben@gmail.com>
2022-10-09 22:16:18 +0300

    app bar color and background image

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,20 @@
 import 'package:flutter/material.dart';
 
-void main() =>runApp(
+void main() {
+  runApp(
   MaterialApp(
-    home:Center(
-       child: Text('Hello Mon amour')
+    home:Scaffold(
+      backgroundColor: Colors.grey,
+      appBar: AppBar(
+        title:Text ("My App Test"), //Title of App Bar
+        backgroundColor: Color(0XFF0DC481),
+        ),
+       body: Center(
+         child: Image(
+          image:NetworkImage('https://designvault.io/wp-content/uploads/2020/06/01-1.png'),
+         ),
+       ),
       )
     )
   );
+}
